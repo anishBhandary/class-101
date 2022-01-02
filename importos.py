3c57756a064474aa5a8b6356b8383c27c6161662
anishBhandary <72250438+anishBhandary@users.noreply.github.com>
2022-01-02 09:06:01 +0530

    Add files via upload

---
+++ importos.py
@@ -0,0 +1,22 @@
+import os
+
+class TransferData:
+ for root, dirs, files in os.walk(file_from):
+   for name in files:
+      print(os.path.join(root, name))
+   for name in dirs:
+      print(os.path.join(root, name))
+      relative_path = os.path.relpath(local_path, file_from)
+      dropbox_path = os.path.join(file_to, relative_path)
+      
+with open(local_path, 'rb',) as f:
+   dbx.files_upload(f.read(),dropbox_path, mode = WriteMode('overwrite'))
+
+     
+      
+
+
+
+        
+
+        
